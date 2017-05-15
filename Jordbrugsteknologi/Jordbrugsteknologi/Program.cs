f37a1dec31427710fc78e225a7ad898df7ee61ae
simon <simon@glashoff.dk>
2017-05-15 11:55:36 +0200

    Create Row fixed

--- Jordbrugsteknologi/Jordbrugsteknologi/Program.cs
+++ Jordbrugsteknologi/Jordbrugsteknologi/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Neo4jClient;
 using System.Collections;
+using System.Diagnostics;
 
 namespace Jordbrugsteknologi
 {
@@ -14,6 +15,8 @@ namespace Jordbrugsteknologi
         GraphClient client;
         List<Field> resultField;
         Row resultRow;
+        Stopwatch timer;
+
 
         static void Main(string[] args)
         {
@@ -23,62 +26,68 @@ namespace Jordbrugsteknologi
         }
         public void Run()
         {
-            //Make the field
-            Field field = new Field("Marken", 2017);
+            timer = new Stopwatch();
+            timer.Start();
+            
+                //Make the field
+                Field field = new Field("Marken2", 2017);
 
-            //Make the different types of weed
-            Weed Crabgrass = new Weed(field.Name, "Crabgrass");
-            Weed Quackgrass = new Weed(field.Name, "Quackgrass");
-            Weed MoringGlory = new Weed(field.Name, "Moring Glory");
-            Weed Pigweed = new Weed(field.Name, "Pigweed");
+                //Make the different types of weed
+                Weed Crabgrass = new Weed(field.Name, "Crabgrass");
+                Weed Quackgrass = new Weed(field.Name, "Quackgrass");
+                Weed MoringGlory = new Weed(field.Name, "Moring Glory");
+                Weed Pigweed = new Weed(field.Name, "Pigweed");
 
-            //Make the different types of herbicide
-            Herbicide Simazine = new Herbicide(field.Name, 5.25, "Simazine");
-            Herbicide Terbuthylazine = new Herbicide(field.Name, 42, "Terbuthylazine");
-            Herbicide Versatil = new Herbicide(field.Name, 2.55, "Versatil");
+                //Make the different types of herbicide
+                Herbicide Simazine = new Herbicide(field.Name, 5.25, "Simazine");
+                Herbicide Terbuthylazine = new Herbicide(field.Name, 42, "Terbuthylazine");
+                Herbicide Versatil = new Herbicide(field.Name, 2.55, "Versatil");
 
-            //Make the different types of crop
-            Crop Wheat = new Crop(field.Name, "Wheat");
+                //Make the different types of crop
+                Crop Wheat = new Crop(field.Name, "Wheat");
 
-            //Make the number of rows in the field
-            Row row1 = new Row(field.Name, 1, Crabgrass, Wheat, Simazine);
-            Row row2 = new Row(field.Name, 2, Crabgrass, Wheat, Terbuthylazine);
-            Row row3 = new Row(field.Name, 3, Crabgrass, Wheat, Versatil);
-            Row row4 = new Row(field.Name, 4, Quackgrass, Wheat, Simazine);
-            Row row5 = new Row(field.Name, 5, Quackgrass, Wheat, Terbuthylazine);
-            Row row6 = new Row(field.Name, 6, Quackgrass, Wheat, Versatil);
-            Row row7 = new Row(field.Name, 7, MoringGlory, Wheat, Simazine);
-            Row row8 = new Row(field.Name, 8, MoringGlory, Wheat, Terbuthylazine);
-            Row row9 = new Row(field.Name, 9, MoringGlory, Wheat, Versatil);
-            Row row10 = new Row(field.Name, 10, Pigweed, Wheat, Simazine);
-            Row row11 = new Row(field.Name, 11, Pigweed, Wheat, Terbuthylazine);
-            Row row12 = new Row(field.Name, 12, Pigweed, Wheat, Versatil);
+                //Make the number of rows in the field
+                Row row1 = new Row(field.Name, 1, Crabgrass, Wheat, Simazine);
+                Row row2 = new Row(field.Name, 2, Crabgrass, Wheat, Terbuthylazine);
+                Row row3 = new Row(field.Name, 3, Crabgrass, Wheat, Versatil);
+                Row row4 = new Row(field.Name, 4, Quackgrass, Wheat, Simazine);
+                Row row5 = new Row(field.Name, 5, Quackgrass, Wheat, Terbuthylazine);
+                Row row6 = new Row(field.Name, 6, Quackgrass, Wheat, Versatil);
+                Row row7 = new Row(field.Name, 7, MoringGlory, Wheat, Simazine);
+                Row row8 = new Row(field.Name, 8, MoringGlory, Wheat, Terbuthylazine);
+                Row row9 = new Row(field.Name, 9, MoringGlory, Wheat, Versatil);
+                Row row10 = new Row(field.Name, 10, Pigweed, Wheat, Simazine);
+                Row row11 = new Row(field.Name, 11, Pigweed, Wheat, Terbuthylazine);
+                Row row12 = new Row(field.Name, 12, Pigweed, Wheat, Versatil);
 
-            //Add all the rows to the list in the field
-            field.rows.Add(row1);
-            field.rows.Add(row2);
-            field.rows.Add(row3);
-            field.rows.Add(row4);
-            field.rows.Add(row5);
-            field.rows.Add(row6);
-            field.rows.Add(row7);
-            field.rows.Add(row8);
-            field.rows.Add(row9);
-            field.rows.Add(row10);
-            field.rows.Add(row11);
-            field.rows.Add(row12);
+                //Add all the rows to the list in the field
+                field.rows.Add(row1);
+                field.rows.Add(row2);
+                field.rows.Add(row3);
+                field.rows.Add(row4);
+                field.rows.Add(row5);
+                field.rows.Add(row6);
+                field.rows.Add(row7);
+                field.rows.Add(row8);
+                field.rows.Add(row9);
+                field.rows.Add(row10);
+                field.rows.Add(row11);
+                field.rows.Add(row12);
 
-            CreateCompleteField(field);
+                //CreateCompleteField(field);
 
-            //Row row13 = new Row(13, Quackgrass, Wheat, Versatil);
+                //Row row13 = new Row(field.Name, 13, Quackgrass, Wheat, Versatil);
+                //CreateRowInField(row13, field);
 
-            //CreateRowInField(row13, field);
+                resultRow = ReadRowInField(row5.Number, field.Name);
 
-            //resultRow = ReadRowInField(row5.Number, field.Name);
+                //resultField = ReadCompleteField(field.Name);
 
-            //resultField = ReadCompleteField(field.Name);
-
-            //Console.ReadKey();
+                //Console.ReadKey();
+            
+            timer.Stop();
+            Console.WriteLine(timer.Elapsed);
+            Console.ReadKey();
         }
         private void Connect()
         {
@@ -140,48 +149,69 @@ namespace Jordbrugsteknologi
 
                     //Opretter crop med relation til den row vi er på
                     client.Cypher
-                        .Merge("(crop:Crop {ID:{ID}, Name:{Name} })")
-                        .OnCreate()
-                        .Set("crop = {tempCrop}")
-                        .WithParams(new { ID = tempCrop.ID, Name = tempCrop.Name, tempCrop })
+                        .Match("(row:Row)")
+                        .Where((Row row) => row.Number == Thisrow.Number && row.ID == Thisrow.ID)
+                        .Create("(row)<-[:PLANTED_IN]-(crop:Crop {tempCrop})")
+                        .WithParam("tempCrop", tempCrop)
                         .ExecuteWithoutResults();
 
-                    client.Cypher
-                        .Match("(row:Row)", "(crop:Crop)")
-                        .Where((Row row) => row.ID == Thisrow.ID && row.Number == Thisrow.Number)
-                        .AndWhere((Crop crop) => crop.ID == tempCrop.ID)
-                        .CreateUnique("(crop)-[:PLANTED_IN]->(row)")
-                        .ExecuteWithoutResults();
+                    //client.Cypher
+                    //    .Merge("(crop:Crop {ID:{ID}, Name:{Name} })")
+                    //    .OnCreate()
+                    //    .Set("crop = {tempCrop}")
+                    //    .WithParams(new { ID = tempCrop.ID, Name = tempCrop.Name, tempCrop })
+                    //    .ExecuteWithoutResults();
+
+                    //client.Cypher
+                    //    .Match("(row:Row)", "(crop:Crop)")
+                    //    .Where((Row row) => row.Number == Thisrow.Number)
+                    //    .AndWhere((Crop crop) => crop.ID == tempCrop.ID)
+                    //    .CreateUnique("(crop)-[:PLANTED_IN]->(row)")
+                    //    .ExecuteWithoutResults();
 
                     //Opretter weed med relation til den row vi er på
                     client.Cypher
-                        .Merge("(weed:Weed {ID:{ID}, Name:{Name} })")
-                        .OnCreate()
-                        .Set("weed = {tempWeed}")
-                        .WithParams(new { ID = tempWeed.ID, Name = tempWeed.Name, tempWeed })
+                        .Match("(row:Row)")
+                        .Where((Row row) => row.Number == Thisrow.Number && row.ID == Thisrow.ID)
+                        .Create("(row)<-[:ATTACKING]-(weed:Weed {tempWeed})")
+                        .WithParam("tempWeed", tempWeed)
                         .ExecuteWithoutResults();
 
-                    client.Cypher
-                        .Match("(row:Row)", "(weed:Weed)")
-                        .Where((Row row) => row.ID == Thisrow.ID && row.Number == Thisrow.Number)
-                        .AndWhere((Weed weed) => weed.ID == tempWeed.ID)
-                        .CreateUnique("(weed)-[:ATTACKING]->(row)")
-                        .ExecuteWithoutResults();
+                    //client.Cypher
+                    //    .Merge("(weed:Weed {ID:{ID}, Name:{Name} })")
+                    //    .OnCreate()
+                    //    .Set("weed = {tempWeed}")
+                    //    .WithParams(new { ID = tempWeed.ID, Name = tempWeed.Name, tempWeed })
+                    //    .ExecuteWithoutResults();
+
+                    //client.Cypher
+                    //    .Match("(row:Row)", "(weed:Weed)")
+                    //    .Where((Row row) => row.Number == Thisrow.Number)
+                    //    .AndWhere((Weed weed) => weed.ID == tempWeed.ID)
+                    //    .CreateUnique("(weed)-[:ATTACKING]->(row)")
+                    //    .ExecuteWithoutResults();
 
                     //Opretter herbicide til den row vi er på
                     client.Cypher
-                        .Merge("(herbicide:Herbicide {ID:{ID}, Dose:{Dose}, Name:{Name} })")
-                        .OnCreate()
-                        .Set("herbicide = {tempHerbicide}")
-                        .WithParams(new { ID = tempHerbicide.ID, Dose = tempHerbicide.Dose, Name = tempHerbicide.Name, tempHerbicide })
+                        .Match("(row:Row)")
+                        .Where((Row row) => row.Number == Thisrow.Number && row.ID == Thisrow.ID)
+                        .Create("(row)<-[:USED_IN]-(herbicide:Herbicide {tempHerbicide})")
+                        .WithParam("tempHerbicide", tempHerbicide)
                         .ExecuteWithoutResults();
 
-                    client.Cypher
-                        .Match("(row:Row)", "(herbicide:Herbicide)")
-                        .Where((Row row) => row.ID == Thisrow.ID && row.Number == Thisrow.Number)
-                        .AndWhere((Herbicide herbicide) => herbicide.ID == tempHerbicide.ID)
-                        .CreateUnique("(herbicide)-[:USED_IN]->(row)")
-                        .ExecuteWithoutResults();
+                    //client.Cypher
+                    //    .Merge("(herbicide:Herbicide {ID:{ID}, Dose:{Dose}, Name:{Name} })")
+                    //    .OnCreate()
+                    //    .Set("herbicide = {tempHerbicide}")
+                    //    .WithParams(new { ID = tempHerbicide.ID, Dose = tempHerbicide.Dose, Name = tempHerbicide.Name, tempHerbicide })
+                    //    .ExecuteWithoutResults();
+
+                    //client.Cypher
+                    //    .Match("(row:Row)", "(herbicide:Herbicide)")
+                    //    .Where((Row row) => row.Number == Thisrow.Number)
+                    //    .AndWhere((Herbicide herbicide) => herbicide.ID == tempHerbicide.ID)
+                    //    .CreateUnique("(herbicide)-[:USED_IN]->(row)")
+                    //    .ExecuteWithoutResults();
                 }
             }
             catch (Exception)
@@ -211,31 +241,38 @@ namespace Jordbrugsteknologi
 
                 //Opretter row med relation til fielden
                 client.Cypher
-                    .Match("(field:Field)")
+                    .Merge("(row:Row {ID:{ID}, Number:{Number} })")
+                    .OnCreate()
+                    .Set("row = {Thisrow}")
+                    .WithParams(new { ID = Thisrow.ID, Number = Thisrow.Number, Thisrow })
+                    .ExecuteWithoutResults();
+
+                client.Cypher
+                    .Match("(field:Field)", "(row:Row)")
                     .Where((Field field) => field.Name == Thisfield.Name)
-                    .Create("(field)-[:CONTAINS]->(row:Row {Number})")
-                    .WithParam("Number", Thisrow)
+                    .AndWhere((Row row) => row.ID == Thisrow.ID)
+                    .CreateUnique("(field)-[:CONTAINS]->(row)")
                     .ExecuteWithoutResults();
                 //Opretter crop med relation til den row vi er på
                 client.Cypher
                     .Match("(row:Row)")
                     .Where((Row row) => row.Number == Thisrow.Number)
-                    .Create("(row)-[:PLANTED_IN]->(crop:Crop {Name})")
-                    .WithParam("Name", tempCrop)
+                    .Create("(row)-[:PLANTED_IN]->(crop:Crop {tempCrop})")
+                    .WithParam("tempCrop", tempCrop)
                     .ExecuteWithoutResults();
                 //Opretter weed med relation til den row vi er på
                 client.Cypher
                     .Match("(row:Row)")
                     .Where((Row row) => row.Number == Thisrow.Number)
-                    .Create("(row)<-[:PLANTED_IN]-(weed:Weed {Name})")
-                    .WithParam("Name", tempWeed)
+                    .Create("(row)<-[:ATTACKING]-(weed:Weed {tempWeed})")
+                    .WithParam("tempWeed", tempWeed)
                     .ExecuteWithoutResults();
                 //Opretter herbicide til den row vi er på
                 client.Cypher
                     .Match("(row:Row)")
                     .Where((Row row) => row.Number == Thisrow.Number)
-                    .Create("(row)<-[:USED_IN]-(herbicide:Herbicide {Dose})")
-                    .WithParam("Dose", tempHerbicide)
+                    .Create("(row)<-[:USED_IN]-(herbicide:Herbicide {tempHerbicide})")
+                    .WithParam("tempHerbicide", tempHerbicide)
                     //.WithParam("Name", tempHerbicide.Name)
                     .ExecuteWithoutResults();
             }
@@ -254,12 +291,6 @@ namespace Jordbrugsteknologi
         {
 
         }
-        /// <summary>
-        /// ReadCompleteField brænder muligvis hvis den finder 2 marker med samme navn. Skal entet gøres noget ved return typen,
-        /// ellers skal foreach looped justeres så der er en work around.
-        /// </summary>
-        /// <param name="searchField"></param>
-        /// <returns></returns>
         public List<Field> ReadCompleteField(string FieldName)
         {
             List<Field> temp = new List<Field>();
@@ -295,22 +326,37 @@ namespace Jordbrugsteknologi
         }
         public Row ReadRowInField(int RowNumber, string FieldName) //Giver mere end 1 row
         {
+            Row tempRow = new Row();
             try
             {
                 Connect();
                 var a = client.Cypher
-                    .Match("(field:Field)-[CONTAINS]->(row)<-[PLANTED_IN]-(crop), (row)<-[ATTACKING]-(weed), (row)<-[USED_IN]-(herbicide)")
+                    .Match("(field:Field)-[CONTAINS]->(row)<-[PLANTED_IN]-(crop)")
                     .Where((Field field) => field.Name == FieldName)
                     .AndWhere((Row row) => row.Number == RowNumber)
-                    .Return((crop, weed, herbicide) =>
-                    new { Crop = crop.As<Crop>(), Weed = weed.As<Weed>(), Herbicide = herbicide.As<Herbicide>() })
+                    .Return((crop) =>
+                    new { Herbicide = crop.As<Herbicide>(), Weed = crop.As<Weed>(), Crop = crop.As<Crop>() })
                     .Results;
 
-                foreach (var item in a)
+                int i = 0;
+                foreach (var item in a) //for(foreach) loop, der nok skal fixes.
                 {
-                    
+                    if (i == 0)
+                    {
+                        tempRow.Herbicide = item.Herbicide;
+                    }
+                    if (i == 1)
+                    {
+                        tempRow.Weed = item.Weed;
+                    }
+                    if (i == 2)
+                    {
+                        tempRow.Crop = item.Crop;
+                    }
+                    i++;
                 }
-                return null;
+
+                return tempRow;
             }
             catch (Exception)
             {
